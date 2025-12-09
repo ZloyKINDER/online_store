132ec4ad731569eff039f881191e91e02bba5135
Timur Gorashchenko <timyr98239718@mail.ru>
2025-12-09 23:00:05 +0300

    add linters

--- config/settings.py
+++ config/settings.py
@@ -12,23 +12,17 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-fqu^zh)+b4^+zc3d(*n$8_f)43x(_n%^qgjm@h*i9l%%@+abqk'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
