90692a48963d265da1074b6a0318cc20549c5774
Timur Gorashchenko <timyr98239718@mail.ru>
2025-12-11 23:59:01 +0300

    add dotenv. settings

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,16 @@
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 from django.conf.global_settings import STATICFILES_DIRS, MEDIA_URL, MEDIA_ROOT
 
+load_dotenv(override=True)
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-fqu^zh)+b4^+zc3d(*n$8_f)43x(_n%^qgjm@h*i9l%%@+abqk'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = True if os.getenv('SECRET_KEY') == 'True' else False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -51,8 +55,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
