57a1b48991f9b668151c741ac17479b8ebb6ad0e
Timur Gorashchenko <timyr98239718@mail.ru>
2026-01-29 02:43:05 +0300

    add validate ban words for products

--- catalog/forms.py
+++ catalog/forms.py
@@ -10,3 +10,17 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ["name", "description", "category", "purchase_price", "image"]
+
+    def clean(self):
+        ban_words = ["казино", "биржа", "обман", "криптовалюта", "дешево",
+                     "полиция", "крипта", "бесплатно", "радар"]
+        cleaned_date = super().clean()
+        name = cleaned_date.get("name")
+        description = cleaned_date.get("description")
+
+        if any(word in name.lower() for word in ban_words):
+            self.add_error("name","Использованны запрещённые слова")
+
+        if any(word in description.lower() for word in ban_words):
+            self.add_error("description","Использованны запрещённые слова")
+
