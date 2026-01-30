23e16881298516c4ac4c24ff8d217483239f32a3
Timur Gorashchenko <timyr98239718@mail.ru>
2026-01-31 00:20:58 +0300

    add style for forms"

--- catalog/forms.py
+++ catalog/forms.py
@@ -7,10 +7,53 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ["name", "description"]
 
+    def __init__(self, *args, **kwargs):
+        super(CategoryForm, self).__init__(*args, **kwargs)
+
+        self.fields["name"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Введите наименование"
+        })
+
+        self.fields["description"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Введите описание"
+        })
+
+
+
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ["name", "description", "category", "purchase_price", "image"]
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+
+        self.fields["name"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Введите наименование"
+        })
+
+        self.fields["description"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Введите описание"
+        })
+
+        self.fields["category"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Выберите категорию"
+        })
+
+        self.fields["purchase_price"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Введите стоимость"
+        })
+
+        self.fields["image"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Загрузите изображение"
+        })
 
     def clean_purchase_price(self):
         purchase_price = self.cleaned_data.get("purchase_price")
